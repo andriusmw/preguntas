954d2972fb6490bc5a6016fd2f150b688056e939
andriusmw <andriusmw@yahoo.es>
2022-05-03 12:20:06 +0200

    remodelado index para figma english

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//Declaramos función para leer la pregunta del json
+
+//llamamos función leer pregunta
+
+//declaramos función para coger las respuestas  y la respuesta correcta (answer)
+//Aquí iremos modificando el DOM, selecionando por let answer = document.getEelementById("answer")
+//y answer.textContent = ---> json.answer
