81fd9e0e6693c28e280ea6d807eb3209be67e94d
andriusmw <andriusmw@yahoo.es>
2022-05-03 12:41:41 +0200

    idea de javascript

--- script.js
+++ script.js
@@ -7,3 +7,27 @@
 //declaramos función para coger las respuestas  y la respuesta correcta (answer)
 //Aquí iremos modificando el DOM, selecionando por let answer = document.getEelementById("answer")
 //y answer.textContent = ---> json.answer
+
+/******************** FUNCIÓN PARA LEER EL JSON *********************************/
+
+function readText(ruta_local) {
+  var texto = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", ruta_local, false);
+  xmlhttp.send();
+  if (xmlhttp.status == 200) {
+    texto = xmlhttp.responseText;
+  }
+  return texto;
+}
+
+/***************************************************************************** */
+
+//Llamo a readtext para ver si lee el json.
+
+function escogerPregunta() {
+  console.log(readText("JSON.json"));
+}
+
+escogerPregunta();
+//Se lee todo el objeto del json, ahora podemos ir llamando por .pregunta , ,answer etc etc
