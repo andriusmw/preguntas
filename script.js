360d56d33c9275f9b86230b9e323ac6d537ff4a9
andriusmw <andriusmw@yahoo.es>
2022-05-05 12:27:54 +0200

    añadido splice a escoger pregunta

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ let base_preguntas = readText("JSON.json");
 let interprete_bp = JSON.parse(base_preguntas);
 //leo el archivo json, lo pasero pasándoselo al interprete_bp
 
-var i = Math.round(Math.random() * 50);
+var longitudArray = 50;
+var i = Math.round(Math.random() * longitudArray);
 //genero número aleatorio entre 0 y 50 para pasárselo al
 //intérprete y que así poder llamar distintos objetos y acceder
 //a distintas preguntas y respuestas.
@@ -46,6 +47,12 @@ function escogerPregunta() {
 
   //Muestra por consola la pregunta y la escribe donde
   //tiene el id question en el documento.
+
+  interprete_bp.splice(i, 1);
+  longitudArray = longitudArray - 1;
+  //Esto hace que después de mostrar la pregunta, la saque del array para que
+  //al volver a llamar a escogerPregunta() no nos pueda salir aleatoriamente la misma.
+  //Restamos 1 para que ahora el número aleatorio sea de 0 a 49
 }
 
 escogerPregunta();
